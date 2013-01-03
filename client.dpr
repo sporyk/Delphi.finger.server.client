d84e2be01320a0cfecefcfcc6372b87771c5edc5
Alex Sporyk <alex.sporik@gmail.com>
2013-01-03 14:14:39 +0400

    Project created

---
+++ client.dpr
@@ -0,0 +1,9 @@
+program client;
+
+{$APPTYPE CONSOLE}
+
+uses SysUtils;
+
+begin
+  // Insert user code here
+end. 
---
+++ server.dpr
@@ -0,0 +1,9 @@
+program server;
+
+{$APPTYPE CONSOLE}
+
+uses SysUtils;
+
+begin
+  // Insert user code here
+end. 
